c26c2b7c625a41ecad6a2bc985ae4ba3a961472c
cloudy <272685110@qq.com>
2017-08-09 11:26:43 +0800

    解决菜单同时展开的问题

--- web/src/page/global/sidebar.vue
+++ web/src/page/global/sidebar.vue
@@ -12,30 +12,24 @@
                     若嵌套需要手动加上上级的path，
                     index要以“/”开头，否则为相对前一个url的地址
                 -->
-                <el-menu theme="dark" :router="true">
-                    <template theme="dark" v-for="(menu,index) in menuList" >
-                        <template v-if="menu.children" >
-                            <el-submenu index="menu.path" :key="index">
-                                <template slot="title"><i :class="menu.Icon"></i>{{menu.name}}</template>
-                                <template v-for="(firstMenu,index) in menu.children"   >
-                                    <template v-if="firstMenu.children"   >
-                                        <el-submenu  >
-                                            <template slot="title"><i :class="firstMenu.Icon"></i>{{firstMenu.name}}</template>
-                                            <template v-for="(secondMenu,index) in firstMenu.children" >
-                                                <el-menu-item v-if="!secondMenu.children" :index="menu.path + '/' + secondMenu.path" :key="secondMenu.index">{{secondMenu.name}}</el-menu-item>
-                                            </template>
-                                        </el-submenu>
+                <el-menu theme="dark" :router="true"  v-for="(menu,index) in menuList" :key="index">
+                    <el-submenu   v-if="menu.children"  index="menu.path">
+                        <template slot="title"><i :class="menu.Icon"></i>{{menu.name}}</template>
+                        <template v-for="(firstMenu,index) in menu.children">
+                            <template v-if="firstMenu.children">
+                                <el-submenu  >
+                                    <template slot="title"><i :class="firstMenu.Icon"></i>{{firstMenu.name}}</template>
+                                    <template v-for="(secondMenu,index) in firstMenu.children" >
+                                        <el-menu-item v-if="!secondMenu.children" :index="menu.path + '/' + secondMenu.path" :key="secondMenu.index">{{secondMenu.name}}</el-menu-item>
                                     </template>
-                                    <template v-if="!firstMenu.children"  >
-                                        <el-menu-item :index="menu.path + '/' + firstMenu.path"><i :class="firstMenu.Icon"></i>{{firstMenu.name}}</el-menu-item>
-                                    </template>
-                                </template>
-                            </el-submenu>
+                                </el-submenu>
+                            </template>
+                            <template v-if="!firstMenu.children"  >
+                                <el-menu-item :index="menu.path + '/' + firstMenu.path"><i :class="firstMenu.Icon"></i>{{firstMenu.name}}</el-menu-item>
+                            </template>
                         </template>
-                        <template v-if="!menu.children" >
-                            <el-menu-item :class="menu.Icon" :index="menu.path"><i :class="menu.Icon"></i>{{menu.name}}</el-menu-item>
-                        </template>
-                    </template>
+                    </el-submenu>
+                    <el-menu-item  v-if="!menu.children" :class="menu.Icon" :index="menu.path"><i :class="menu.Icon"></i>{{menu.name}}</el-menu-item>
                 </el-menu>
             </el-col>
         </el-row>
