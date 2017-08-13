ea27cfa534a1b296eaa6374e3d4f29932b1af815
cloudy <272685110@qq.com>
2017-08-13 19:04:07 +0800

    移动端后台

---
+++ web_mobile/src/views/common/footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <section id="bottom-guide">
+        <div>footer</div>
+    </section>
+</template>
+<script>
+    import {mapState} from 'vuex'
+    export default {
+    	data(){
+            return{
+                
+            }
+        },
+         
+    }
+</script>
+<style lang="scss" scoped>
+    #bottom-guide{
+        background-color: #fff;
+        position: fixed;
+        z-index: 100;
+        bottom:0;
+    }
+</style>
---
+++ web_mobile/src/views/front/global/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    
+        <head-top></head-top>
+        <foot-bottom></foot-bottom>
+     </div>
+</template>
+<script>
+    import {default as headTop} from '../../common/header'
+    import {default as footBottom} from '../../common/footer'
+    import {mapState } from 'vuex'
+    export default {
+    	data(){
+            return{
+                
+            }
+        },
+        components:{
+            headTop,
+            footBottom
+        }
+         
+    }
+</script>
+<style lang="scss" scoped>
+     
+</style>
