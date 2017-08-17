2ad9090fa5c32488b6c93da643d659477ce19be4
cloudy <272685110@qq.com>
2017-08-17 15:35:54 +0800

    detail显示

--- web_pc/src/views/admin/address/cityForm.vue
+++ web_pc/src/views/admin/address/cityForm.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>City form</div>
+    <div>
+        <el-form label-position="left"  class="demo-table-expand">
+          <el-form-item label="商品名称">
+            <span>123</span>
+          </el-form-item>
+          <el-form-item label="所属店铺">
+            <span>123</span>
+          </el-form-item>
+          <el-form-item label="商品 ID">
+            <span>123</span>
+          </el-form-item>
+          <el-form-item label="店铺 ID">
+            <span>123</span>
+          </el-form-item>
+          <el-form-item label="商品分类">
+            <span>1231</span>
+          </el-form-item>
+          <el-form-item label="店铺地址">
+            <span>店铺地址</span>
+          </el-form-item>
+          <el-form-item label="商品描述">
+            <span>商品描述</span>
+          </el-form-item>
+        </el-form>
+    </div>
 </template>
+<script>
+     
+    import { mapState } from 'vuex';
+    export default {
+      data() {
+        return {
+            
+        }
+    }
+     
+    }
+</script>
